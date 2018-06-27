db0ce9ce689e700fa806ca0fefbea7ac8707d7d8
Joseph Todd <joseph.todd@ucdenver.edu>
2018-06-27 14:33:43 -0600

    Initial Commit

---
+++ Directory/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Directory.Models;
+using Microsoft.Extensions.Options;
+
+namespace Directory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Links links;
+
+        public HomeController(IOptions<Links> options)
+        {
+            this.links = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            return View(links);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Directory/Models/Links.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Directory.Models
+{
+    public class Links
+    {
+        public IEnumerable<Link> StandaloneWebApps { get; set; }
+        public IEnumerable<Link> LtiConfigs { get; set; }
+        public IEnumerable<Link> Other { get; set; }
+    }
+
+    public class Link
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
